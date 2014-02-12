85da7196b0f831dcde0eb08e39d198e634335ea6
Logan Hasson <logan.hasson@gmail.com>
2014-02-12 11:51:37 -0500

    Some specs passing

--- backer.rb
+++ backer.rb
@@ -1,14 +1,8 @@
 class Backer
-  attr_reader :name, :backed_projects
+  attr_reader :name
 
   def initialize(name)
     @name = name
-    @backed_projects = []
-  end
-
-  def back_project(project)
-    backed_projects << project
-    project.backers << self
   end
 
 end
--- project.rb
+++ project.rb
@@ -1,14 +1,7 @@
 class Project
-  attr_reader :title, :backers
 
   def initialize(title)
     @title = title
-    @backers = []
-  end
-
-  def add_backer(backer)
-    backers << backer
-    backer.backed_projects << self
   end
 
 end
