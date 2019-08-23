26573cefb7a8480a831bf1d38a65f642d32af7d2
maxwellbenton <maxwellbenton@gmail.com>
2019-08-23 13:13:21 -0400

    updates tests, includes project_backer join class

--- spec/kickstarter_spec.rb
+++ spec/kickstarter_spec.rb
@@ -1,32 +1,54 @@
 describe 'Backer - ::new' do
   it 'takes a name on initialization, accessible through an attribute reader' do
-    backer = Backer.new("Avi")
-    expect(backer.name).to eq("Avi")
-  end
-
-  it 'initializes with a @backed_projects attribute, an empty array' do
-    backer = Backer.new("Avi")
-    expect(backer.backed_projects).to eq([])
+    backer = Backer.new('Avi')
+    expect(backer.name).to eq('Avi')
   end
 end
 
-
 describe 'Project - ::new' do
   it 'takes a title on initialization, accessible through an attribute reader' do
-    project = Project.new("Project With So Much Amaze")
-    expect(project.title).to eq("Project With So Much Amaze")
+    project = Project.new('Project With So Much Amaze')
+    expect(project.title).to eq('Project With So Much Amaze')
+  end
+end
+
+describe 'ProjectBacker' do
+  describe '@@all' do
+    it 'is a class variable set to an empty array' do
+      expect(ProjectBacker.class_variable_get(:@@all)).to eq([])
+    end
   end
 
-  it 'initializes with a @backers attribute, an empty array' do
-    project = Project.new("Project With So Much Amaze")
-    expect(project.backers).to eq([])
+  describe '.all' do
+    it 'is a class method that returns the @@all class variable' do
+      ProjectBacker.class_variable_set(:@@all, ["I'm sorry, Jon"])
+      expect(ProjectBacker.all).to eq(["I'm sorry, Jon"])
+    end
+  end
+
+  describe '::new' do
+    it 'takes in a project and a backer on initialization, accessible through an attribute reader' do
+      amaze_project = Project.new('Project With So Much Amaze')
+      avi_backer = Backer.new('Avi')
+      project_backer = ProjectBacker.new(amaze_project, avi_backer)
+      expect(project_backer.project).to eq(amaze_project)
+      expect(project_backer.backer).to eq(avi_backer)
+    end
+
+    it 'stores the new instance of ProjectBacker in the @@all class variable' do
+      ProjectBacker.class_variable_set(:@@all, [])
+      amaze_project = Project.new('Project With So Much Amaze')
+      avi_backer = Backer.new('Avi')
+      project_backer = ProjectBacker.new(amaze_project, avi_backer)
+      expect(ProjectBacker.all).to eq([project_backer])
+    end
   end
 end
 
 describe 'Backer - #back_project' do
-  it 'accepts a Project as an argument and stores it in a backed_projects array' do
-    spencer = Backer.new("Spencer")
-    magic = Project.new("Magic The Gathering Thing")
+  it 'is an instance method that accepts a project as an argument and creates a ProjectBacker, associating the project with this backer' do
+    spencer = Backer.new('Spencer')
+    magic = Project.new('Magic The Gathering Thing')
 
     # If we are calling this method in this way, what type of argument is it taking?
     # We are actually passing in a Project object! Cool, huh?
@@ -36,9 +58,9 @@ describe 'Backer - #back_project' do
 end
 
 describe 'Project - #add_backer' do
-  it 'accepts a Backer as an argument and stores it in a backers array' do
-    book = Project.new("Ruby, Ruby, and More Ruby")
-    steven = Backer.new("Steven")
+  it 'is an instance method that accepts a backer as an argument and creates a ProjectBacker, associating the backer with this project' do
+    book = Project.new('Ruby, Ruby, and More Ruby')
+    steven = Backer.new('Steven')
 
     # Same here. We are actually passing around a Backer object. This is pretty
     # simple functionality, but objects can interact with one another in really
@@ -46,29 +68,5 @@ describe 'Project - #add_backer' do
     book.add_backer(steven)
 
     expect(book.backers).to include(steven)
-  end
-end
-
-describe 'Backer - More Advanced #back_project' do
-  it 'also adds the backer to the project\'s backers array' do
-    logan = Backer.new("Logan")
-    hoverboard = Project.new("Awesome Hoverboard")
-    logan.back_project(hoverboard)
-
-    # If by this point, the project knows about a new backer, where does that mean
-    # some more magic needs to happen? There's really only one place it can happen.
-    expect(hoverboard.backers).to include(logan)
-  end
-end
-
-describe 'Project - More Advanced #add_backer' do
-  it 'also adds the project to the backer\'s backed_projects array' do
-    ropes = Project.new("All The Ropes")
-    arel = Backer.new("Arel")
-    ropes.add_backer(arel)
-
-    # Same thing here. There's only one place we can tell arel about his newly
-    # backed project. Where does that need to happen?
-    expect(arel.backed_projects).to include(ropes)
   end
 end
